bbc988fd11f6f2c05659749dbb4c639af1fa2fc3
Michael Young <michael25young25@gmail.com>
2022-08-05 16:03:51 -0400

    trying something else...

--- client/src/components/navigation/footer.stories.tsx
+++ client/src/components/navigation/footer.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { Footer } from './Footer'
+import { Footer } from '@/components/Navigation/Footer'
 
 const story = {
     title: 'Navigation/Footer',
