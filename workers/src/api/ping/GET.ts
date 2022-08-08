4270426fa2876598dea4535c2495564c257a31c7
Michael Young <michael25young25@gmail.com>
2022-08-08 18:12:02 -0400

    removing all infra

--- workers/src/api/ping/GET.ts
+++
@@ -1,7 +0,0 @@
-import { IRouteHandler } from "../../router";
-
-export class PingRouteGetHandler implements IRouteHandler {
-    public async handle(): Promise<Response> {
-        return new Response("pong", { status: 200 })
-    }
-}
--- workers/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from './router'
-
-addEventListener('fetch', async (event) => {
-  const response = new Router().handle(event.request)
-  event.respondWith(response)
-})
