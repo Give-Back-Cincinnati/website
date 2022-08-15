a0871f4fe91b6699a639909f454d4a1093e22b72
Michael Young <michael@inspiringservice.org>
2022-08-15 11:21:22 -0400

    early ff page

---
+++ src/pages/fall-feast/index.tsx
@@ -0,0 +1,33 @@
+import { NextPage } from "next"
+import styles from './index.module.scss'
+import { HorizontalBreak, LeftDashedBorder } from '@/components/Backgrounds'
+
+
+const FallFeast: NextPage = () => {
+    return <div>
+        <HorizontalBreak style={{ marginBottom: '2rem', marginTop: '2rem' }}>
+                Fall Feast
+        </HorizontalBreak>
+        <LeftDashedBorder>
+            <div className={styles.description}>
+                <p>
+                    In 2005 our Give Back Cincinnati team began serving and sharing a Thanksgiving meal - Fall Feast.  The hope was to take time out of the busy holiday to join as a community in celebration of gratitude.
+                </p>
+                <p>
+                    We are excited to join together as a community this Thanksgiving,
+                    <span className={styles.highlight}>
+                        Thursday, November 24, 2022 
+                    </span> at the Duke Energy Convention Center.
+                </p>
+                <p>
+                    Fall Feast has a mission to change our community for the better through celebration, connection, and the passion to serve others. This calling has led to the rapid growth of one of the largest Thanksgiving meals in the region. With the help of local businesses, neighbors, and community members, this event has blossomed into a true community celebration that oﬀers more than a meal. 
+                </p>
+            </div>
+        </LeftDashedBorder>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/bhJ0zcyK2DA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+}
+
+export default FallFeast
