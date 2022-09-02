c62e28cc353713dce1f6ab3aa27269fcf35bb9ea
Michael Young <michael25young25@gmail.com>
2022-09-02 09:11:52 -0400

    updated event details to take and format data

--- src/components/Events/EventDetails/index.stories.tsx
+++ src/components/Events/EventDetails/index.stories.tsx
@@ -6,10 +6,15 @@ import { EventDetails } from './index'
 const story = {
     title: 'Events/Details',
     component: EventDetails,
+    args: {
+        startTime: '2022-08-24T13:00:00.000+00:00',
+        endTime: '2022-08-24T17:00:00.000+00:00',
+        address: 'The Globe Covington, 12 E 5th Street, Covington, KY 41011'
+    }
 } as ComponentMeta<typeof EventDetails>
 
 export default story
 
-const Template: ComponentStory<typeof EventDetails> = (args) => <EventDetails />
+const Template: ComponentStory<typeof EventDetails> = (args) => <EventDetails {...args} />
 
 export const Primary = Template.bind({})
--- src/components/Events/EventDetails/index.tsx
+++ src/components/Events/EventDetails/index.tsx
@@ -1,21 +1,39 @@
+import { useMemo } from 'react'
+import { DateTime } from 'luxon'
+
 import styles from './index.module.scss'
 
-export const EventDetails = () => {
+interface EventDetails {
+    startTime: string,
+    endTime: string,
+    address: string
+}
+
+export const EventDetails = ({ startTime, endTime, address }: EventDetails) => {
+    const { timeString, dateString } = useMemo(() => {
+        const startDateTimeLuxon = DateTime.fromISO(startTime)
+        const endDateTimeLuxon = DateTime.fromISO(endTime)
+        const timeString = `${startDateTimeLuxon.toLocaleString(DateTime.TIME_SIMPLE)} - ${endDateTimeLuxon.toLocaleString(DateTime.TIME_SIMPLE)}`
+        const dateString = startDateTimeLuxon.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
+        
+        return { timeString, dateString}
+    }, [ startTime, endTime])
+
     return <div className={styles.container}>
         <h2>
             EVENT DETAILS
         </h2>
         <div className={styles.detail}>
             <span>Date:</span>
-            <span>Thursday, December 6th</span>
+            <span>{ dateString }</span>
         </div>
         <div className={styles.detail}>
             <span>Time:</span>
-            <span>7pm - 10pm</span>
+            <span>{ timeString }</span>
         </div>
         <div className={styles.detail}>
             <span>Location:</span>
-            <span>The Globe Covington, 12 E 5th Street, Covington, KY 41011</span>
+            <span>{ address }</span>
         </div>
         
     </div>
