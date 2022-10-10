8afd1479f94fd2ab5166e2ef3f204258af685cfd
Michael Young <michael25young25@gmail.com>
2022-10-10 16:14:31 -0400

    iframe implementation

--- src/components/Utils/DonateModal/index.tsx
+++ src/components/Utils/DonateModal/index.tsx
@@ -10,26 +10,12 @@ type Props = {
     campaign?: string
 }
 
-let x = true
-
 export const DonateModal = ({ campaign = '', isOpen, onRequestClose }: Props) => {
-    const iframeRef = useRef<HTMLIFrameElement | null>(null)
-
-    useEffect(() => {
-        if(x && iframeRef !== null && iframeRef.current && iframeRef.current.contentDocument) {
-            const el = document.createElement('div')
-            el.innerHTML = `<!-- Begin Give Lively Fundraising Widget -->
-            <script>gl=document.createElement('script');gl.src='https://secure.givelively.org/widgets/branded_donation/give-back-cincinnati.js';document.getElementsByTagName('head')[0].appendChild(gl);</script><div data-widget-src='https://secure.givelively.org/donate/give-back-cincinnati?ref=sd_widget' id="give-lively-widget" class="gl-branded-donation-widget"></div>
-            <!-- End Give Lively Fundraising Widget -->`
-            iframeRef.current.contentDocument.body.appendChild(el)
-            x = false
-        }
-    }, [ campaign ])
 
     return <Modal isOpen={isOpen} onRequestClose={onRequestClose} className={styles.modalContainer} paperPadding={0}>
         <ErrorBoundary>
             <>
-            <iframe src="about:blank" width="100%" style={{ minHeight: "90vh", border: 'none' }} ref={iframeRef}></iframe>
+                <iframe width='100%' style={{ minHeight: '90vh', border: 'none' }} src="/donation_iframe.html?campaign=fall-feast-2022"></iframe>
                 {/* <script
                     src={`https://secure.givelively.org/widgets/branded_donation/give-back-cincinnati${campaign}.js`}
                     async
