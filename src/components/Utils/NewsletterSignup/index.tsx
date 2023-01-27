8691d6b0849c710aa609e26925101a4b8ce667b5
Michael Young <michael25young25@gmail.com>
2023-01-26 19:39:43 -0500

    Fix CLS error on newsletter signup (#9)
    * first try for newsletter signup
    
    * type errors...
    
    * don't get fancy
    
    * trigger nightly build at 6am UTC
    
    * working newsletter signup
    
    * change loading behavior to prevent content shift

--- src/components/Utils/NewsletterSignup/index.tsx
+++ src/components/Utils/NewsletterSignup/index.tsx
@@ -2,23 +2,15 @@ import React, { useState, useEffect, useRef } from 'react'
 
 import { useGetSchema } from 'hooks'
 
+import { Spinner } from '@/components/DataDisplay'
 import { DynamicForm } from '@/components/DynamicForm'
 import { Button, Modal } from '@/components/Utils'
 
 export const NewsletterSignup = () => {
     const formRef = useRef<HTMLFormElement | null>(null)
     const [ isModalOpen, setModalOpen ] = useState(false)
-    const [ isLoading, setLoading ] = useState(true)
-
-    useEffect(() => {
-        setLoading(false)
-    }, [])
 
     const EmailSignupSchema = useGetSchema('EmailSignup')
-
-    if (isLoading || !EmailSignupSchema) {
-        return null
-    }
 
     function toggleModal () {
         setModalOpen(!isModalOpen)
@@ -46,11 +38,15 @@ export const NewsletterSignup = () => {
                 <>
                     <h2 style={{ textAlign: 'center' }}>Sign up for our Newsletter!</h2>
                     <form ref={formRef} method='post' action="https://givebackcincinnati.us12.list-manage.com/subscribe/post?u=160bf2c9daa8af34307358b18&amp;id=0992f3a90c&amp;f_id=00d941e0f0">
-                        <DynamicForm
-                            entity={EmailSignupSchema}
-                            onSubmit={handleSubscription}
-                            isProtected
-                        />
+                        {
+                            EmailSignupSchema
+                                ? <DynamicForm
+                                    entity={EmailSignupSchema}
+                                    onSubmit={handleSubscription}
+                                    isProtected
+                                />
+                                : <Spinner />
+                        }
                     </form>
                 </>
             </Modal>
