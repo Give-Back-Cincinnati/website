c78c9b8f12b6409417943e8aca3720f3de773737
Michael Young <michael25young25@gmail.com>
2023-07-31 19:41:15 -0400

    try next-on-pages

--- src/app/admin/events/[id]/page.tsx
+++ src/app/admin/events/[id]/page.tsx
@@ -12,7 +12,7 @@ import styles from './[_id].module.scss'
 export async function generateStaticParams () {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/events`)
     const events: Events[] = await res.json()
-    return events.map(event => ({ _id: event._id }))
+    return events.map(event => ({ id: event._id }))
 }
 
 const AdminEventDetails = (props: { params: { id: string }}) => {
