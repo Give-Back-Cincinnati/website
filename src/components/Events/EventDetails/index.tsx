07c1b4c0e26973908fcc47c1d920bbbd7bd28c8e
Michael Young <michael25young25@gmail.com>
2023-08-14 21:43:02 -0400

    App/events (#28)
    * transition more to app folder
    
    * update landing page to trim event description if its too long
    
    * optimize pages for as app components
    
    * explicitly set time zone

--- src/components/Events/EventDetails/index.tsx
+++ src/components/Events/EventDetails/index.tsx
@@ -1,40 +1,45 @@
-import { useMemo } from 'react'
-import { DateTime } from 'luxon'
+import { useMemo } from "react";
+import { DateTime } from "luxon";
 
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 
 interface EventDetails {
-    startTime: string,
-    endTime: string,
-    address: string
+  startTime: string;
+  endTime: string;
+  address: string;
 }
 
 export const EventDetails = ({ startTime, endTime, address }: EventDetails) => {
-    const { timeString, dateString } = useMemo(() => {
-        const startDateTimeLuxon = DateTime.fromISO(startTime)
-        const endDateTimeLuxon = DateTime.fromISO(endTime)
-        const timeString = `${startDateTimeLuxon.toLocaleString(DateTime.TIME_SIMPLE)} - ${endDateTimeLuxon.toLocaleString(DateTime.TIME_SIMPLE)}`
-        const dateString = startDateTimeLuxon.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
-        
-        return { timeString, dateString}
-    }, [ startTime, endTime])
+  const { timeString, dateString } = useMemo(() => {
+    const startDateTimeLuxon =
+      DateTime.fromISO(startTime).setZone("America/New_York");
+    const endDateTimeLuxon =
+      DateTime.fromISO(endTime).setZone("America/New_York");
+    const timeString = `${startDateTimeLuxon.toLocaleString(
+      DateTime.TIME_SIMPLE
+    )} - ${endDateTimeLuxon.toLocaleString(DateTime.TIME_SIMPLE)}`;
+    const dateString = startDateTimeLuxon.toLocaleString(
+      DateTime.DATE_MED_WITH_WEEKDAY
+    );
 
-    return <div className={styles.container}>
-        <h2>
-            EVENT DETAILS
-        </h2>
-        <div className={styles.detail}>
-            <span>Date:</span>
-            <span>{ dateString }</span>
-        </div>
-        <div className={styles.detail}>
-            <span>Time:</span>
-            <span>{ timeString }</span>
-        </div>
-        <div className={styles.detail}>
-            <span>Location:</span>
-            <span>{ address }</span>
-        </div>
-        
+    return { timeString, dateString };
+  }, [startTime, endTime]);
+
+  return (
+    <div className={styles.container}>
+      <h2>EVENT DETAILS</h2>
+      <div className={styles.detail}>
+        <span>Date:</span>
+        <span>{dateString}</span>
+      </div>
+      <div className={styles.detail}>
+        <span>Time:</span>
+        <span>{timeString}</span>
+      </div>
+      <div className={styles.detail}>
+        <span>Location:</span>
+        <span>{address}</span>
+      </div>
     </div>
-}
+  );
+};
