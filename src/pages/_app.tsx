7637c35aa7011b26b53130f0344953b02f5a2115
Michael Young <michael25young25@gmail.com>
2023-10-24 23:22:00 -0400

    add new dt picker, needs more testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,58 +1,71 @@
-import '../styles/globals.scss'
-import React from 'react'
-import Head from 'next/head'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
-import { Navigation } from '@/components/Navigation'
-import { Footer } from '@/components/Footer'
-import { Toaster } from '@/components/DataDisplay/Toaster'
+import "../styles/globals.scss";
+import React from "react";
+import Head from "next/head";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { Navigation } from "@/components/Navigation";
+import { Footer } from "@/components/Footer";
+import { Toaster } from "@/components/DataDisplay/Toaster";
 
-import { ThemeProvider } from '@mui/material/styles'
-import { CreateThemeHook } from 'hooks'
-import { store, actions } from 'store'
-import { Provider } from 'react-redux'
-import { createServices } from 'services'
-createServices(store, actions)
+import { ThemeProvider } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+import { CreateThemeHook } from "hooks";
+import { store, actions } from "store";
+import { Provider } from "react-redux";
+import { createServices } from "services";
+createServices(store, actions);
 
 export type NextPageWithLayout = NextPage & {
-    getLayout?: (page: JSX.Element)
-        => React.ReactNode
-}
-  
+  getLayout?: (page: JSX.Element) => React.ReactNode;
+};
+
 type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-    const getLayout = Component.getLayout ?? ((page) => page)
-    const theme = CreateThemeHook()
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const theme = CreateThemeHook();
 
-    return <ThemeProvider theme={theme}>
+  return (
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterLuxon}>
         <Provider store={store}>
-            <Head>
-                <title>Give Back Cincinnati</title>
-                <meta name='description' content="Give Back Cincinnati's Website" />
-                <link rel="icon" href="/favicon-16x16.ico" sizes="16x16" />
-                <link rel="icon" href="/favicon-32x32.ico" sizes="32x32" />
-                <link rel="icon" href="/favicon-96x96.ico" sizes="96x96" />
-                <link rel="preload" href='/AlternateGotNo3D_Regular.ttf' as="font" type="font/ttf" />
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-            </Head>
+          <Head>
+            <title>Give Back Cincinnati</title>
+            <meta name="description" content="Give Back Cincinnati's Website" />
+            <link rel="icon" href="/favicon-16x16.ico" sizes="16x16" />
+            <link rel="icon" href="/favicon-32x32.ico" sizes="32x32" />
+            <link rel="icon" href="/favicon-96x96.ico" sizes="96x96" />
+            <link
+              rel="preload"
+              href="/AlternateGotNo3D_Regular.ttf"
+              as="font"
+              type="font/ttf"
+            />
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
 
-            <Navigation />
+          <Navigation />
 
-            <main style={{ paddingTop: 93, minHeight: 'calc(100vh - 93px)' }}>
-                {
-                    // @ts-ignore
-                    getLayout(<Component {...pageProps} />)
-                }
-            </main>
+          <main style={{ paddingTop: 93, minHeight: "calc(100vh - 93px)" }}>
+            {
+              // @ts-ignore
+              getLayout(<Component {...pageProps} />)
+            }
+          </main>
 
-            <Footer />
+          <Footer />
 
-            <Toaster />
+          <Toaster />
         </Provider>
+      </LocalizationProvider>
     </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
