d55e5d561212435213b916a5658635f516b97000
Bjorn Kluwe <bjoern.kluwe@gmail.com>
2024-08-06 17:07:26 -0400

    do client-side validation that all required elements are filled

--- src/app/events/[slug]/registration.tsx
+++ src/app/events/[slug]/registration.tsx
@@ -5,6 +5,7 @@ import { HorizontalBreak } from "@/components/Backgrounds";
 import { DynamicForm } from "@/components/DynamicForm";
 import { Spinner, Notify } from "@/components/DataDisplay";
 import { Alert } from "@/components/Utils";
+import { EntitySchema } from "@/types/schema";
 import {
   Events,
   useGetMeQuery,
@@ -20,10 +21,26 @@ export default function Registration({ event }: { event: Events }) {
   const guestRegistrationSchema = useGetSchema("GuestRegistration");
   const { data: me } = useGetMeQuery();
 
+  const [errMessage, setErrMessage] = React.useState<string>("");
+
   const [submitRegistration, { status, error, reset, isSuccess }] =
     usePostEventsByEventIdRegisterMutation();
 
-  function handleRegistrationSubmit(itm: Record<string, unknown>) {
+  function handleRegistrationSubmit(itm: Record<string, unknown>, schema: EntitySchema) {
+
+    // loop through all the required elements and check that the registration object 
+    // being sent contains all necessary data - if no, tell user they need to fill in all 
+    // required elements
+    for (const requiredElement of schema.required) {
+      if (!itm[requiredElement] && (Object.keys(schema.properties)).includes(requiredElement)) {
+        setErrMessage(`You haven't filled in all the required elements!`)
+        return;
+      }
+    }
+
+    // if we are here, our check has passed, set error message to empty string
+    // and submit registration
+    setErrMessage("");
     submitRegistration({
       eventId: event._id,
       body: itm as GuestRegistration,
@@ -35,7 +52,7 @@ export default function Registration({ event }: { event: Events }) {
       Notify({ title: "Successfully Registered!", intent: "positive" });
     }
 
-    if (error) {
+    if (error) {      
       reset();
     }
   }, [reset, error, status]);
@@ -77,6 +94,7 @@ export default function Registration({ event }: { event: Events }) {
     return schema;
   }, [me, userRegistrationSchema, guestRegistrationSchema, eventData]);
 
+
   if (eventData && "isFull" in eventData) {
     return (
       <div>
@@ -91,18 +109,30 @@ export default function Registration({ event }: { event: Events }) {
     <div>
       <HorizontalBreak>Registration</HorizontalBreak>
 
-      {isSuccess && (
-        <Alert
-          key="registrationSuccess"
-          title="You have successfully registered for this event"
-          intent="positive"
-        />
-      )}
+      {
+        errMessage && (
+          <Alert
+            key="registrationFailure"
+            title={errMessage}
+            intent="negative"
+          />
+        )
+      }
+
+      {
+        isSuccess && (
+          <Alert
+            key="registrationSuccess"
+            title="You have successfully registered for this event"
+            intent="positive"
+          />
+        )
+      }
 
       {registrationSchema && !isSuccess ? (
         <DynamicForm
           entity={registrationSchema}
-          onSubmit={handleRegistrationSubmit}
+          onSubmit={(itm) => handleRegistrationSubmit(itm, registrationSchema)}
           values={me} // if we add new fields in the future, autopopulate them for logged in users
           hiddenFields={["user"]}
           customFields={eventData ? event.customFields: {}} // pass custom fields from event data
